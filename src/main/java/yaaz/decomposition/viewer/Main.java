9ccbb88d51ca722aca9080193bbc7e77d75aaf1a
YaaZ <764610@gmail.com>
2020-03-12 16:24:57 +0300

    Moved Frame creation into SwingUtilities.invokeLater

--- src/main/java/yaaz/decomposition/viewer/Main.java
+++ src/main/java/yaaz/decomposition/viewer/Main.java
@@ -2,17 +2,19 @@ package yaaz.decomposition.viewer;
 
 import yaaz.decomposition.viewer.rendering.RenderingMode;
 
+import javax.swing.*;
+
 
 public class Main {
 
 
     public static void main(String[] args) {
-        RenderingMode renderingMode = RenderingMode.VULKAN;
-        if(args.length == 1 && args[0].equalsIgnoreCase("graphics2d")) renderingMode = RenderingMode.GRAPHICS2D;
-
         JNI.load(); // We want to force JNI library loading at startup to fail ASAP if it doesn't exist
 
-        new Frame(renderingMode).setVisible(true);
+        RenderingMode renderingMode = args.length == 1 && args[0].equalsIgnoreCase("graphics2d") ?
+                RenderingMode.GRAPHICS2D : RenderingMode.VULKAN;
+
+        SwingUtilities.invokeLater(() -> new Frame(renderingMode).setVisible(true));
     }
 
 
